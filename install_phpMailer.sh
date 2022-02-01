0a92d86f25fe43d63d464a38d832241aef1782eb
Teddy <teddy.gauthier@outlook.com>
2022-02-01 12:58:01 +0100

    fix : add PHPMailer in Lib fix : remove PHPMailer form Dockerfile

---
+++ install_phpMailer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+version="6.5.3"
+wget https://github.com/PHPMailer/PHPMailer/archive/refs/tags/v${version}.tar.gz
+tar xfvz v${version}.tar.gz -C www/Lib/
+rm v${version}.tar.gz
+mv www/Lib/PHPMailer-${version} www/Lib/PHPMailer
