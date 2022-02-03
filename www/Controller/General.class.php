a3d23bf1fd082c00be2226b24889e80e91cfa0ff
teddy <teddy.gauthier@outlook.com>
2022-02-03 23:22:39 +0100

    feat : add PHPMailerManager

--- www/Controller/General.class.php
+++ www/Controller/General.class.php
@@ -3,12 +3,17 @@
 namespace App\Controller;
 
 use App\Core\View;
+use App\Core\PHPMailerManager;
 
 class General{
 
     public function home()
     {
         echo "Welcome";
+
+        /* $email = new PHPMailerManager(); */
+        /* $retour = $email->send('teddy.gauthier@outlook.com', 'email de test', 'je suis un email de test'); */
+        /* echo $retour ? 'email envoyer' : 'erreur'; */
     }
 
     public function contact()
