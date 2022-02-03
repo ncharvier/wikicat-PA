2f87f9992b8a5b166e2414d665f4c4d4877caeef
teddy <teddy.gauthier@outlook.com>
2022-02-03 12:31:58 +0100

    fix : install_phpMailer.sh

--- install_phpMailer.sh
+++ install_phpMailer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 version="6.5.3"
+[ ! -d www/Lib ] && mkdir www/Lib
 wget https://github.com/PHPMailer/PHPMailer/archive/refs/tags/v${version}.tar.gz
 tar xfvz v${version}.tar.gz -C www/Lib/
 rm v${version}.tar.gz
