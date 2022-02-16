b63116feb84ab635fd9d2a503e8be0a1936d151b
teddy <teddy.gauthier@outlook.com>
2022-02-16 23:24:32 +0100

    feat : component

--- www/Assets/demo/Components/Card.php
+++ www/Assets/demo/Components/Card.php
@@ -1,8 +1,37 @@
 <div class="card">
     <img class="card-image" src="http://localhost/Assets/images/test-img.jpeg">
-    <h1 class="card-title">title</h1>
-    <p class="card-text">
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-    </p>
-    <button class="btn btn--primary">test btn</div>
+    <div class="card-body">
+        <h2 class="card-title">title</h2>
+        <p class="card-text">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        </p>
+        <button class="btn btn--primary">test btn</button>
+    </div>
+</div>
+<br>
+<div class="card">
+    <div class="card-body">
+        <h2 class="card-title">title</h2>
+        <p class="card-text">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        </p>
+        <button class="btn btn--primary">test btn</button>
+    </div>
+</div>
+<br>
+<div class="card">
+    <div class="card-body">
+        <p class="card-text">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        </p>
+        <button class="btn btn--primary">test btn</button>
+    </div>
+</div>
+<br>
+<div class="card">
+    <div class="card-body">
+        <p class="card-text">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        </p>
+    </div>
 </div>
