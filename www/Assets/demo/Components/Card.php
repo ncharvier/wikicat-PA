09b0d29e67f5ef584a9e9f27f782e08ae5ebb3ae
teddy <teddy.gauthier@outlook.com>
2022-02-17 23:03:58 +0100

    feature : card component

--- www/Assets/demo/Components/Card.php
+++ www/Assets/demo/Components/Card.php
@@ -1,7 +1,26 @@
 <div class="card">
-    <img class="card-image" src="http://localhost/Assets/images/test-img.jpeg">
+    <img class="card-image" src="http://localhost/Assets/images/WAP.jpeg">
     <div class="card-body">
-        <h2 class="card-title">title</h2>
+        <h2 class="card-title">Rejoindre la WAP</h2>
+        <button class="btn btn--primary">S'engager</button>
+    </div>
+</div>
+<br>
+<div class="card card--sm">
+    <img class="card-image" src="http://localhost/Assets/images/WAP.jpeg">
+    <div class="card-body">
+        <h2 class="card-title">je suis un titre</h2>
+        <p class="card-text">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        </p>
+        <button class="btn btn--primary">test btn</button>
+    </div>
+</div>
+<br>
+<div class="card card--lg">
+    <img class="card-image" src="http://localhost/Assets/images/WAP.jpeg">
+    <div class="card-body">
+        <h2 class="card-title">je suis un titre</h2>
         <p class="card-text">
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
         </p>
@@ -11,7 +30,7 @@
 <br>
 <div class="card">
     <div class="card-body">
-        <h2 class="card-title">title</h2>
+        <h2 class="card-title">je suis un titre</h2>
         <p class="card-text">
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
         </p>
