c9593140ea6cf9cd85785f278f1a289dea958bce
teddy <teddy.gauthier@outlook.com>
2022-02-25 22:16:54 +0100

    feat : line on left of tree component

--- www/Assets/demo/Components/Tree.php
+++ www/Assets/demo/Components/Tree.php
@@ -2,16 +2,32 @@
     <li>
         <span class="tree-title"><span class="tree-arrow tree-down"></span>tree title</span>
         <ul class="tree-nested active">
-            <li>info 1</li>
+            <li>
+                <span class="tree-title"><span class="tree-arrow"></span>info 1</span>
+                <ul class="tree-nested">
+                    <li>info 1 - 1</li>
+                    <li>info 1 - 2</li>
+                    <li>info 1 - 3</li>
+                    <li class="last">info 1 - 4</li>
+                </ul>
+            </li>
             <li>info 2</li>
             <li>info 3</li>
             <li>
                 <span class="tree-title"><span class="tree-arrow"></span>info 4</span>
                 <ul class="tree-nested">
                     <li>info 4 - 1</li>
-                    <li>info 4 - 2</li>
+                    <li>
+                        <span class="tree-title"><span class="tree-arrow"></span>info 4 - 2</span>
+                        <ul class="tree-nested">
+                            <li>info 4 - 2 - 1</li>
+                            <li>info 4 - 2 - 2</li>
+                            <li>info 4 - 2 - 3</li>
+                            <li class="last">info 4 - 2 - 4</li>
+                        </ul>
+                    </li>
                     <li>info 4 - 3</li>
-                    <li>info 4 - 4</li>
+                    <li class="last">info 4 - 4</li>
                 </ul>
             </li>
         </ul>
