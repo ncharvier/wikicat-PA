be02edd859413b3f3fb27217c5c6f8287984db72
Nicolas <67713953+tshadow42@users.noreply.github.com>
2022-02-27 17:30:27 +0100

    wip

--- www/View/back/dashboard.view.php
+++ www/View/back/dashboard.view.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-6 p-0">
+    <div class="col-12 col-lg-6 p-0">
         <section class="main-section">
             <section>
                 <h2>Statistiques utilisateur :</h2>
@@ -25,11 +25,11 @@
             <hr>
             <section>
                 <h2>Historique des visites :</h2>
-                <canvas id="dashboardChart" width="400" height="300"></canvas>
+                <canvas class="my-2" id="dashboardChart" width="400" height="200"></canvas>
             </section>
         </section>
     </div>
-    <div class="col-6 p-0">
+    <div class="col-12 col-lg-6 p-0">
         <section class="main-section">
             <h2>État du serveur :</h2>
             <section class="row">
@@ -72,7 +72,20 @@
             </section>
         </section>
         <section class="main-section">
-
+            <div class="row">
+                <div class="col-6">
+                    <h2>Nouvelles Pages</h2>
+                </div>
+                <div class="col-6">
+                    <h2>Total Pages</h2>
+                </div>
+                <div class="col-6">
+                    <h2>Pages supprimé</h2>
+                </div>
+                <div class="col-6">
+                    <h2>Pages mise à jour</h2>
+                </div>
+            </div>
         </section>
     </div>
 </div>
