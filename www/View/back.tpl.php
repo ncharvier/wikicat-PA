cf69e6b7fea7ed3ba6c2c6747d4db8bfba757125
Nicolas <67713953+tshadow42@users.noreply.github.com>
2022-02-27 15:14:20 +0100

    page change show on side menu

--- www/View/back.tpl.php
+++ www/View/back.tpl.php
@@ -18,19 +18,19 @@
         </header>
         <div class="row">
             <nav id="main-nav" class="col-2 p-0">
-                <a href="/admin/dashboard" class="main-nav-choice active">
+                <a href="/admin/dashboard" class="main-nav-choice <?= $activePage=="dashboard"?"active":""?>">
                     <span class="main-nav-icon"><i class="fas fa-chart-line"></i></span>
                     <span class="main-nav-title">Dashboard</span>
                 </a>
-                <a href="/admin/user" class="main-nav-choice">
+                <a href="/admin/user" class="main-nav-choice <?= $activePage=="user"?"active":""?>">
                     <span class="main-nav-icon"><i class="fas fa-users"></i></span>
                     <span class="main-nav-title">Utilisateurs</span>
                 </a>
-                <a href="/admin/role" class="main-nav-choice">
+                <a href="/admin/role" class="main-nav-choice <?= $activePage=="role"?"active":""?>">
                     <span class="main-nav-icon"><i class="fas fa-key"></i></span>
                     <span class="main-nav-title">Rôles</span>
                 </a>
-                <div class="main-nav-choice" data-wc-target="main-nav-subchoice-page">
+                <div class="main-nav-choice <?= $activePage=="page"?"active":""?>" data-wc-target="main-nav-subchoice-page">
                     <span class="main-nav-icon"><i class="fas fa-file"></i></span>
                     <span class="main-nav-title">Pages</span>
                     <i class="fas fa-chevron-down subchoice-indicator"></i>
@@ -39,19 +39,19 @@
                     <a href="/admin/pageList" class="main-nav-subchoice">Liste pages</a>
                     <a href="/admin/pageTemplate" class="main-nav-subchoice">Templates</a>
                 </div>
-                <a href="/admin/comment" class="main-nav-choice">
+                <a href="/admin/comment" class="main-nav-choice <?= $activePage=="comment"?"active":""?>">
                     <span class="main-nav-icon"><i class="fas fa-comments"></i></span>
                     <span class="main-nav-title">Commentaires</span>
                 </a>
-                <a href="/admin/visualSetting" class="main-nav-choice">
+                <a href="/admin/visualSetting" class="main-nav-choice <?= $activePage=="visualSetting"?"active":""?>">
                     <span class="main-nav-icon"><i class="fas fa-paint-roller"></i></span>
                     <span class="main-nav-title">Apparence</span>
                 </a>
-                <a href="/admin/plugin" class="main-nav-choice">
+                <a href="/admin/plugin" class="main-nav-choice <?= $activePage=="plugin"?"active":""?>">
                     <span class="main-nav-icon"><i class="fas fa-puzzle-piece"></i></span>
                     <span class="main-nav-title">Plugins</span>
                 </a>
-                <a href="/admin/globalSetting" class="main-nav-choice">
+                <a href="/admin/globalSetting" class="main-nav-choice <?= $activePage=="globalSetting"?"active":""?>">
                     <span class="main-nav-icon"><i class="fas fa-cogs"></i></span>
                     <span class="main-nav-title">Paramètres</span>
                 </a>
