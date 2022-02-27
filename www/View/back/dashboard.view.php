9963f9111bf395298ae209c82d9ed54dae9161fe
Nicolas <67713953+tshadow42@users.noreply.github.com>
2022-02-27 15:48:29 +0100

    add server state bars

--- www/View/back/dashboard.view.php
+++ www/View/back/dashboard.view.php
@@ -4,20 +4,20 @@
             <section>
                 <h2>Statistique utilisateur :</h2>
                 <div class="row">
-                    <div class="col-6 my-1">
-                        <i class="fas fa-user-friends"></i>
+                    <div class="col-6 my-1 d-flex align-items-center">
+                        <i class="fas fa-user-friends fa-2x"></i>
                         <div class="badge badge--pill badge--success ml-3"> + 159</div>
                     </div>
-                    <div class="col-6 my-1">
-                        <i class="fas fa-user-plus"></i>
+                    <div class="col-6 my-1 d-flex align-items-center">
+                        <i class="fas fa-user-plus fa-2x"></i>
                         <div class="badge badge--pill badge--success ml-3"> + 59</div>
                     </div>
-                    <div class="col-6 my-1">
-                        <i class="fas fa-comment"></i>
+                    <div class="col-6 my-1 d-flex align-items-center">
+                        <i class="fas fa-comment fa-2x"></i>
                         <div class="badge badge--pill badge--warning ml-3"> + 6</div>
                     </div>
-                    <div class="col-6 my-1">
-                        <i class="fas fa-comment-slash"></i>
+                    <div class="col-6 my-1 d-flex align-items-center">
+                        <i class="fas fa-comment-slash fa-2x"></i>
                         <div class="badge badge--pill badge--danger ml-3"> + 63</div>
                     </div>
                 </div>
@@ -34,13 +34,37 @@
             <h2>État du serveur :</h2>
             <section class="row">
                 <div class="col-12">
-                    <i class="fas fa-hdd"></i> Stockage
+                    <div class="d-flex justify-content-space-between align-items-center">
+                        <i class="fas fa-hdd fa-2x"></i>
+                        <div class="d-flex flex-col" style="width: 90%">
+                            <span>Stockage</span>
+                            <div class="progress">
+                                <div class="progress-bar progress--degradee-success" style="width: 21%"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-12">
-                    <i class="fas fa-microchip"></i> CPU
+                    <div class="d-flex justify-content-space-between align-items-center">
+                        <i class="fas fa-microchip fa-2x"></i>
+                        <div class="d-flex flex-col" style="width: 90%">
+                            <span>CPU</span>
+                            <div class="progress">
+                                <div class="progress-bar progress--degradee-danger" style="width: 98%"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-12">
-                    <i class="fas fa-memory"></i> RAM
+                    <div class="d-flex justify-content-space-between align-items-center">
+                        <i class="fas fa-memory fa-2x"></i>
+                        <div class="d-flex flex-col" style="width: 90%">
+                            <span>RAM</span>
+                            <div class="progress">
+                                <div class="progress-bar progress--degradee-warning" style="width: 80%"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-12">
                     Fluidité du site :
