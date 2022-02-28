43edeee5e94f01f143ac5356d57cd4fa6a049978
Nicolas <67713953+tshadow42@users.noreply.github.com>
2022-02-28 15:40:00 +0100

    creation of the WikiPage controller and model

--- www/Controller/WikiPage.class.php
+++ www/Controller/WikiPage.class.php
@@ -2,37 +2,15 @@
 
 namespace App\Controller;
 
-use App\Core\queryBuilder;
-use App\Core\View;
-use App\Model\WikiPage as page;
-
 class WikiPage
 {
-    public function show(string $pageTitle)
+    public function show()
     {
-        $page = new page();
-        $page = $page->foundByTitle($pageTitle);
 
-        if ($page == null){
-            header('Location: /w/edit/'.$pageTitle);
-        }
-
-        echo $pageTitle;
     }
 
-    public function edit(string $pageTitle)
+    public function edit()
     {
-        $page = new page();
-        $page = $page->foundByTitle($pageTitle);
 
-        if ($page == null){
-            $page = new page();
-        }
-        $view = new View("front/pageEdit", "front");
-        $view->assign("title", $pageTitle);
-
-
-
-        //echo "edit " . $pageTitle;
     }
 }
--- www/Model/WikiPage.class.php
+++ www/Model/WikiPage.class.php
@@ -7,24 +7,4 @@ use App\Core\BaseSQL;
 class WikiPage extends BaseSQL
 {
 
-    protected $id = null;
-    protected $title;
-    protected $parentPageId;
-    protected $createdAt;
-    protected $updateAt;
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function foundByTitle($title): ?object
-    {
-        return parent::getFromValue(strtolower(trim($title)), "title");
-    }
-
-    public function save()
-    {
-        parent::save();
-    }
 }
