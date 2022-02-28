94ebd68a5de28590fd212afa86889a3c39d38a45
teddy <teddy.gauthier@outlook.com>
2022-02-28 22:57:20 +0100

    feat : page visual setting

--- www/View/back/visualSetting.view.php
+++ www/View/back/visualSetting.view.php
@@ -1,1 +1,15 @@
-<?php
+<div class="col-12 p-0">
+    <section class="main-section">
+        <h2>Choix du thème</h2>
+        <button class="btn btn--sm btn--outline-primary">Selectionner</button>
+        <button class="btn btn--sm btn--outline-primary">Modifier</button>
+        <button class="btn btn--sm btn--outline-primary">Supprimer</button>
+        <button class="btn btn--sm btn--outline-primary">Nouveau</button>
+        <button class="btn btn--sm btn--outline-primary">Dupliquer</button>
+        <button class="btn btn--sm btn--outline-primary">Importer</button>
+        <button class="btn btn--sm btn--outline-primary">Exporter</button>
+    </section>
+    <section class="main-section">
+        test
+    </section>
+</div>
