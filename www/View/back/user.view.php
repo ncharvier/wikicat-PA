cfeca3c40ad43d4d469d458bc1cfeaaf6f874607
nicolas <charvier.nicolas@gmail.com>
2022-03-01 22:38:55 +0100

    test utils

--- www/View/back/user.view.php
+++ www/View/back/user.view.php
@@ -14,36 +14,36 @@
                 <tbody>
                 <tr class="table-row">
                     <td>Picon Daniel</td>
-                    <td>Admin, Defaut</td>
-                    <td>Actif</td>
+                    <td><span style="color: #D22D3D;">Admin</span>, <span style="color: #625BC1;">Defaut</span></td>
+                    <td class="text-success">Actif</td>
                     <td>daniel.picon@gmail.com</td>
                     <td><button class="btn btn--primary">Editer</button></td>
                 </tr>
                 <tr class="table-row">
                     <td>Picon Damien</td>
-                    <td>Defaut</td>
-                    <td>Banni</td>
+                    <td><span style="color: #625BC1;">Defaut</span></td>
+                    <td class="text-danger">Banni</td>
                     <td>picon.damien@gmail.com</td>
                     <td><button class="btn btn--primary">Editer</button></td>
                 </tr>
                 <tr class="table-row">
                     <td>Lebreton Dany</td>
-                    <td>Editeur, Defaut</td>
-                    <td>Non vérifier</td>
+                    <td><span style="color: #2E8B77;">Editeur</span>, <span style="color: #625BC1;">Defaut</span></td>
+                    <td class="text-warning">Non vérifier</td>
                     <td>dany.lebreton@gmail.com</td>
                     <td><button class="btn btn--primary">Editer</button></td>
                 </tr>
                 <tr class="table-row">
                     <td>Orty Deny</td>
-                    <td>Modo, Defaut</td>
-                    <td>Actif</td>
+                    <td><span style="color: #FA8654;">Modo</span>, <span style="color: #625BC1;">Defaut</span></td>
+                    <td class="text-success">Actif</td>
                     <td>orty.deny@gmail.com</td>
                     <td><button class="btn btn--primary">Editer</button></td>
                 </tr>
                 <tr class="table-row">
                     <td>Dupon Dorian</td>
-                    <td>Defaut</td>
-                    <td>Actif</td>
+                    <td><span style="color: #625BC1;">Defaut</span></td>
+                    <td class="text-success">Actif</td>
                     <td>dupon.dorian@gmail.com</td>
                     <td><button class="btn btn--primary">Editer</button></td>
                 </tr>
