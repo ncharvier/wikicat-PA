6641f9ee5d613ea92bfe00ed8b3795d1a07ebb76
nicolas <charvier.nicolas@gmail.com>
2022-05-24 22:59:17 +0200

    Some fix

--- www/View/auth.tpl.php
+++ www/View/auth.tpl.php
@@ -14,8 +14,7 @@
                     <div class="col"></div>
                     <div class="col-5">
                         <h1 class="text-center m-0">
-                            <span class="text-weight-800">Wiki</span>
-                            <span class="text-secondary text-weight-400">cat</span>
+                            <span class="text-weight-800">Wiki</span><span class="text-secondary text-weight-400">cat</span>
                         </h1>
                         <?php include $this->view.".view.php";?>
                     </div>
