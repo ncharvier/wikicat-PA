6068f6fe8c9ff4bf3544e02ca5cedbd55423288d
Nicolas <charvier.nicolas@gmail.com>
2022-06-30 16:22:03 +0200

    Wiki page rooting and query builder

--- www/Controller/WikiPage.class.php
+++ www/Controller/WikiPage.class.php
@@ -2,15 +2,37 @@
 
 namespace App\Controller;
 
+use App\Core\queryBuilder;
+use App\Core\View;
+use App\Model\WikiPage as page;
+
 class WikiPage
 {
-    public function show()
+    public function show(string $pageTitle)
     {
+        $page = new page();
+        $page = $page->foundByTitle($pageTitle);
 
+        if ($page == null){
+            header('Location: /w/edit/'.$pageTitle);
+        }
+
+        echo $pageTitle;
     }
 
-    public function edit()
+    public function edit(string $pageTitle)
     {
+        $page = new page();
+        $page = $page->foundByTitle($pageTitle);
 
+        if ($page == null){
+            $page = new page();
+        }
+        $view = new View("front/pageEdit", "front");
+        $view->assign("title", $pageTitle);
+
+
+
+        //echo "edit " . $pageTitle;
     }
 }
