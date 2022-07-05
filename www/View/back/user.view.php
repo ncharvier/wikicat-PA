ae7b01e12201d80909ba10fa3b0fab614fe3b9a5
teddy <teddy.gauthier@outlook.com>
2022-07-05 22:13:08 +0200

    fix : status of user

--- www/View/back/user.view.php
+++ www/View/back/user.view.php
@@ -16,7 +16,21 @@
                 <tr class="table-row">
                     <td><?=$user->login?></td>
                     <td><span style="color: #D22D3D;">Admin</span>, <span style="color: #625BC1;">Defaut</span></td>
-                    <td class="text-success"><?=$user->status?></td>
+                    <td class="text-success">
+<?php
+switch ($user->status) {
+    case 0:
+        echo "non activer";
+        break;
+    case 1:
+        echo "activer";
+        break;
+    case 2:
+        echo "supprimer";
+        break;
+}
+?>
+                    </td>
                     <td><?=$user->email?></td>
                     <td><button class="btn btn--primary">Editer</button></td>
                 </tr>
